f3a9b446acdc3f640fa6e3a43b1e03ba36fa3031
cewfowler <cewfowler@gmail.com>
2020-12-19 16:20:56 -0600

    First commit

---
+++ ex2/costFunction.m
@@ -0,0 +1,25 @@
+function [J, grad] = costFunction(theta, X, y)
+%COSTFUNCTION Compute cost and gradient for logistic regression
+%   J = COSTFUNCTION(theta, X, y) computes the cost of using theta as the
+%   parameter for logistic regression and the gradient of the cost
+%   w.r.t. to the parameters.
+
+% Initialize some useful values
+m = length(y); % number of training examples
+hx = sigmoid(X*theta);
+
+
+% Calculate the cost function
+J = (1/m) * sum(log(hx) .* (-y) - log(1 - hx) .* (1-y));
+
+% Calculate the gradient
+grad = 1/(m) * (X' * (hx - y));
+
+
+
+
+
+
+% =============================================================
+
+end
